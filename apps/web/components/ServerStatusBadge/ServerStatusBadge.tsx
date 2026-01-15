3873f1003cc4ca0cd9b588e26903c256ca7d505a
Marques Scripps <hello@mscripps.com>
2026-01-15 23:49:51 +0000

    chore: Removed files that aren't being used.

--- apps/web/components/ServerStatusBadge/ServerStatusBadge.tsx
+++ apps/web/components/ServerStatusBadge/ServerStatusBadge.tsx
@@ -1,11 +1,32 @@
 import { Server } from "@/lib/api.types";
 import React from "react";
+import { TextureBadge } from "@workspace/ui/components/TextureBadge/TextureBadge";
 
 interface ServerStatusBadgeProps {
   server: Server | null;
 }
 
+const convertServerStatusToBadgeVariant = (status: string) => {
+    switch (status) {
+        case "running":
+        return "success";
+        case "stopped":
+        return "destructive";
+        case "starting":
+        case "stopping":
+        return "warning";
+        default:
+        return "primary";
+    }
+}
+
 const ServerStatusBadge: React.FunctionComponent<ServerStatusBadgeProps> = ({ server }) => {
-  return <div>{server?.status}</div>;
+  return (
+      <TextureBadge
+          variant={convertServerStatusToBadgeVariant(server?.status.toLowerCase() || "unknown")}
+      >
+        {server?.status}
+      </TextureBadge>
+  )
 };
 export default ServerStatusBadge;
